615dbab4adc4f310e6e21a90c5e29cc68196a292
Casper Kuijjer <casper@kuijjer.com>
2020-04-12 15:34:40 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const slsw = require('serverless-webpack')
+const ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+module.exports = {
+  context: process.cwd(), // to automatically find tsconfig.json
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  target: 'node',
+  entry: slsw.lib.entries,
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, '.webpack'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      eslint: true,
+    }),
+    new ForkTsCheckerNotifierWebpackPlugin({
+      title: 'TypeScript',
+      excludeWarnings: false,
+      skipSuccessful: true,
+    }),
+  ],
+  externals: 'aws-sdk',
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: [{ loader: 'ts-loader', options: { transpileOnly: true } }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+}
